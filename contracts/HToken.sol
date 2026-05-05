9b5ea77c9c9ce49a7489d38a6c2c227e530237fc
Arulselvan-65 <arulselvan2244@gmail.com>
2026-05-06 01:47:31 +0530

    Updated changes

--- contracts/HToken.sol
+++ contracts/HToken.sol
@@ -8,7 +8,6 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract HToken is ERC20, Ownable {
 
     uint256 public immutable MAX_SUPPLY;
-    uint256 public currentSupply;
 
     constructor(uint256 _maxSupply)
         ERC20("HToken", "HTK")
@@ -19,19 +18,11 @@ contract HToken is ERC20, Ownable {
 
     error ExceedsMintLimit();
     error InsufficientBalance();
-    event TokenTransferred(address indexed from, address indexed to, uint256 amount);
+    event TokenMinted(address indexed to, uint256 amount);
 
-    function mint(uint256 amount) public onlyOwner {
-        if(MAX_SUPPLY < (amount + currentSupply)) revert ExceedsMintLimit();
-        _mint(msg.sender, amount);
-        currentSupply += amount;
-    }
-
-    function transfer(address to, uint256 amount) public override returns (bool) {
-        address sender = msg.sender;
-        if(balanceOf(sender) < amount) revert InsufficientBalance();
-        _transfer(sender, to, amount);
-        emit TokenTransferred(sender, to, amount);
-        return true;
+    function mint(address to, uint256 amount) public onlyOwner {
+        if(MAX_SUPPLY < (amount + totalSupply())) revert ExceedsMintLimit();
+        _mint(to, amount);
+        emit TokenMinted(to, amount);
     }
 }
--- test/HToken.js
+++ test/HToken.js
@@ -6,7 +6,7 @@ const { expect } = chai;
 describe("HToken", function () {
 
   async function deployHTokenFixture() {
-    const MAX_SUPPLY = 1_000_000n;
+    const MAX_SUPPLY = ethers.parseEther("1000000");
     const [owner, otherAccount] = await ethers.getSigners();
 
     const Token = await ethers.getContractFactory("HToken");
@@ -15,6 +15,17 @@ describe("HToken", function () {
   }
 
   describe("Deployment", function () {
+
+    it("Should set the right token name", async function () {
+      const { token } = await loadFixture(deployHTokenFixture);
+      expect(await token.name()).to.equal("HToken");
+    });
+
+    it("Should set the right token symbol", async function () {
+      const { token } = await loadFixture(deployHTokenFixture);
+      expect(await token.symbol()).to.equal("HTK");
+    });
+
     it("Should set the right max supply", async function () {
       const { token, MAX_SUPPLY } = await loadFixture(deployHTokenFixture);
       expect(await token.MAX_SUPPLY()).to.equal(MAX_SUPPLY);
@@ -30,13 +41,13 @@ describe("HToken", function () {
 
     it("Should allow owner to mint tokens", async function () {
       const { token, owner } = await loadFixture(deployHTokenFixture);
-      await token.mint(5000n);
-      expect(await token.balanceOf(owner.address)).to.equal(5000n);
+      await token.mint(owner.address, ethers.parseEther("5000"));
+      expect(await token.balanceOf(owner.address)).to.equal(ethers.parseEther("5000"));
     });
 
     it("Should throw error when users mint", async function () {
       const { token, otherAccount } = await loadFixture(deployHTokenFixture);
-      expect(token.connect(otherAccount).mint(100n))
+      await expect(token.connect(otherAccount).mint(otherAccount.address, 100n))
         .to
         .revertedWithCustomError(token, "OwnableUnauthorizedAccount")
         .withArgs(otherAccount.address);
@@ -44,14 +55,14 @@ describe("HToken", function () {
 
     it("Should allow to mint tokens if currentSupply + amount ≤ maxSupply", async function () {
       const { token, owner } = await loadFixture(deployHTokenFixture);
-      await token.mint(1_000_000n);
-      expect(await token.balanceOf(owner.address)).to.equal(1_000_000n);
+      await token.mint(owner.address, ethers.parseEther("1000000"));
+      expect(await token.balanceOf(owner.address)).to.equal(ethers.parseEther("1000000"));
     });
 
     it("Should throw error if currentSupply + amount > maxSupply", async function () {
       const { token, owner } = await loadFixture(deployHTokenFixture);
-      await token.mint(1_000_000n);
-      await expect(token.mint(1n))
+      await token.mint(owner.address, ethers.parseEther("1000000"));
+      await expect(token.mint(owner.address, 1n))
         .to
         .revertedWithCustomError(token, "ExceedsMintLimit");
     });
@@ -59,19 +70,40 @@ describe("HToken", function () {
 
   describe("Transfer", function () {
 
-    it("Should transfer tokens if sender balance <= requested amount", async function () {
-      const { token, otherAccount } = await loadFixture(deployHTokenFixture);
-      await token.mint(1_000_000n);
-      await token.transfer(otherAccount.address, 6000n)
-      expect(await token.balanceOf(otherAccount.address)).to.equal(6000n);
+    it("Should transfer tokens if sender balance >= requested amount", async function () {
+      const { token, owner, otherAccount } = await loadFixture(deployHTokenFixture);
+      await token.mint(owner.address, ethers.parseEther("1000000"));
+      await token.transfer(otherAccount.address, ethers.parseEther("6000"))
+      expect(await token.balanceOf(otherAccount.address)).to.equal(ethers.parseEther("6000"));
     });
 
     it("Should throw error if insufficient balance", async function () {
-      const { token, otherAccount } = await loadFixture(deployHTokenFixture);
-      await token.mint(5000n);
-      expect(token.transfer(otherAccount.address, 6000n))
+      const { token, owner, otherAccount } = await loadFixture(deployHTokenFixture);
+      await token.mint(owner.address, ethers.parseEther("5000"));
+      await expect(token.transfer(otherAccount.address, ethers.parseEther("6000")))
         .to
-        .revertedWithCustomError(token, "InsufficientBalance");
+        .revertedWithCustomError(token, "ERC20InsufficientBalance")
+        .withArgs(owner.address, token.balanceOf(owner.address), ethers.parseEther("6000"));
+    });
+  });
+
+  describe("Events", function () {
+
+    it("Should emit TokenMinted on token mint", async function () {
+      const { token, owner } = await loadFixture(deployHTokenFixture);
+      expect(await token.mint(owner.address, ethers.parseEther("5000")))
+        .to
+        .emit(token, "TokenMinted")
+        .withArgs(owner.address, ethers.parseEther("5000"));
+    });
+
+    it("Should emit Transfer on token transfer", async function () {
+      const { token, owner, otherAccount } = await loadFixture(deployHTokenFixture);
+      await token.mint(owner.address, ethers.parseEther("7000"));
+      await expect(token.transfer(otherAccount.address, ethers.parseEther("6000")))
+        .to
+        .emit(token, "Transfer")
+        .withArgs(owner.address, otherAccount.address, ethers.parseEther("6000"));
     });
   });
 });
